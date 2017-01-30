4914b7421522481400bb32cd5da92e7b8ea377c9
IcyFreakz <wolfyfreakz@hotmail.com>
2017-01-31 04:24:15 +0800

    Transitions of scenes, Options done and Dialogue done

---
+++ IDP_Ass2/Assets/Scripts/Mal/Dialogue/DialogueText.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Text))]
+[RequireComponent (typeof (RectTransform))]
+
+public class DialogueText : MonoBehaviour 
+{
+	public float typeSpeed;
+	public float startDelay;
+	public float volumeVariation;
+	public bool startOnAwake;
+
+	private int counter;
+	private string textToType;
+	private bool typing;
+	private Text textComp;
+    //string[] DialogueText1 = new string[] { "1. Laik's super awesome custom typewriter script 2. You can click to skip to the next text", "3.All text is stored in a single string array", "4. Ok, now we can continue", "5. End Kappa" };
+    //string[] DialogueText2 = new string[] { "asdasdasd click to skip to the next text", "3.All text is stored in a single string array", "4. Ok, now we can continue", "5. End Kappa" };
+    void Awake()
+	{ 
+		textComp = GetComponent<Text>();
+	
+
+		counter = 0;
+		textToType = textComp.text;
+		textComp.text = "";
+
+		if(startOnAwake)
+		{
+			InvokeRepeating("Type", startDelay, typeSpeed);
+		}
+	}
+
+	public void StartTyping()
+	{	
+		if(!typing)
+		{
+			InvokeRepeating("Type", startDelay, typeSpeed);
+		}
+		else
+		{
+			print(gameObject.name + " : Is already typing!");
+		}
+	}
+
+	public void StopTyping()
+	{
+		typing = false;
+		CancelInvoke("Type");
+	}
+
+	private void Type()
+	{	
+		typing = true;
+		textComp.text = textComp.text + textToType[counter];
+		counter++;
+
+
+		if(counter == textToType.Length)
+		{	
+			typing = false;
+			CancelInvoke("Type");
+
+        }
+	}
+}
---
+++ IDP_Ass2/Assets/Scripts/Mal/MainMenu/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public Canvas MainCanvas;
+    public Canvas OptionsCanvas;
+
+    void Awake()
+    {
+        OptionsCanvas.enabled = false;
+    }
+
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = true;
+        MainCanvas.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        SceneManager.LoadScene("Dialogue");
+    }
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}

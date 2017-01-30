8a5447f46ca3069cf4306cbd7da7aab54193b291
Hidayat <hidhandsome143@gmail.com>
2017-01-31 05:58:54 +0800

    Adding Animation files to GameUI

---
+++ IDP_Ass2/Assets/Scripts/Hid/Shop/ConsumableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsumableObject : ShopObject {
+
+    string DescriptionOfItem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void ShowOnScreen()
+    {
+        base.ShowOnScreen();
+        description.text = DescriptionOfItem;
+    }
+}

790a47b2601da3c6ca119115194e905721b9604b
Hidayat <hidhandsome143@gmail.com>
2017-01-30 14:30:52 +0800

    MY UPDATED THINGS

---
+++ IDP_Ass2/Assets/Scripts/ShopBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ShopBehavior : MonoBehaviour {
+
+    public Button Weapons, Armour, Consumables, Sell;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ IDP_Ass2/Assets/Scripts/ShopButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShopButtonBehavior : MonoBehaviour {
+
+    public Toggle toggler;
+
+    Vector3 OriginalScale, ToggledScale;
+
+	// Use this for initialization
+	void Start () {
+        OriginalScale = transform.localScale;
+        ToggledScale = transform.localScale + new Vector3(0.5f, 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (toggler.isOn)
+        {
+            transform.localScale = ToggledScale;
+        }
+        else
+        {
+            transform.localScale = OriginalScale;
+        }
+	}
+}
